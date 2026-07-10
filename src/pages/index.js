4306599d3b106f3394f1c4b70de430bae12ab7d5
gyanesh <gyanesh5009@gmail.com>
2026-07-11 00:17:30 +0530

    case-sensitive issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import HomePage from "./HomePage";
 import PlayGround from "./PlayGround";
 import SudokuDocs from "./games/SudokuDocs";
 import About from "./About";
-import Roadmap from "./RoadMap";
+import Roadmap from "./Roadmap";
 import Docs from "./docs";
 import SchulteDocs from "./games/SchulteDocs";
 
